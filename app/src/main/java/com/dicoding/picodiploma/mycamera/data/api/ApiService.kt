2c0251405a4c99ac1154113bf9b0092d001cf299
ArmizaRahmaddion <armizarahmaddion08@gmail.com>
2024-11-15 20:43:47 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/data/api/ApiService.kt
@@ -0,0 +1,16 @@
+package com.dicoding.picodiploma.mycamera.data.api
+
+//kontrak untuk menangani endpoint yang dibutuhkan dalam ApiService.
+
+import okhttp3.MultipartBody
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface ApiService {
+    @Multipart
+    @POST("skin-cancer/predict")
+    suspend fun uploadImage(
+        @Part file: MultipartBody.Part
+    ): FileUploadResponse
+}
